8181a135935104234068144df9e05b5ab239f27e
croxq4 <croxq4@gmail.com>
2017-02-06 14:43:40 -0500

    belajar koding

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="simpan.php" method="POST">
+<strong><H4>MASUKAN DATA ANDA</H4></strong><br>
+<table>
+<tr>
+	<td>Nama</td><td>:</td> 
+	<td><input type="text" name="nama"></td>
+</tr>
+<tr>
+	<td>Nama Pengguna</td><td>:</td> 
+	<td><input type="text" name="username"></td>
+</tr>
+<tr>
+	<td>Kata Sandi</td><td>:</td> 
+	<td><input type="text" name="pass"></td>
+</tr>
+<tr>
+	<td>Alamat E-mail</td><td>:</td> 
+	<td><input type="text" name="email"></td>
+</tr>
+<tr><td><input type = "SUBMIT" value="SIMPAN"></td></tr>
+
+</table>
+
+</form>
+</body>
+</html>
